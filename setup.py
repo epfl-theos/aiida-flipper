81063d742594a57081437a94ae0a0975c644f4be
Leonid Kahle <leonid.kahle@epfl.ch>
2017-07-07 17:22:06 +0200

    Added setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import json
+
+if __name__ == '__main__':
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
+    setup(
+        include_package_data=True, packages=find_packages(), setup_requires=['reentry'], reentry_register=True,
+        **kwargs
+    )
