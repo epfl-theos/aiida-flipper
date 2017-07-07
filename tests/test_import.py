7bf39f359fee09aa206868d35a1af2cdec93a43a
Leonid Kahle <leonid.kahle@epfl.ch>
2017-07-07 19:12:09 +0200

    added gitignore

---
+++ tests/test_import.py
@@ -0,0 +1,14 @@
+#CalculationFactory('quantumespresso.pw')
+from aiida.parsers import ParserFactory
+FlipperCalculation = CalculationFactory('quantumespresso.flipper')
+CalculationFactory('quantumespresso.hustler')
+ParserFactory('quantumespresso.flipper')
+calc = FlipperCalculation()
+print calc._plugin_type_string
+
+#~ from aiida.orm.calculation.job.quantumespresso import flipper
+#~ from aiida.orm.calculation.job.quantumespresso.flipper import FlipperCalculation
+#~ from aiida.orm.calculation.job.quantumespresso.hustler import HustlerCalculation
+#~ from aiida.orm.parsers.plugins.quantumespresso.flipper import FlipperParser
+
+
