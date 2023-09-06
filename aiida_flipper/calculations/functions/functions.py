6106236d95ae9e4c850a2cd85b2b2735fce2f291
tsthakur <tushar_thakur@outlook.com>
2023-09-06 18:18:59 +0200

    Added new function to get last step of trajectory For AIMD run, the last step is taken from the pinball trajectory, which this function delivers.

--- aiida_flipper/calculations/functions/functions.py
+++ aiida_flipper/calculations/functions/functions.py
@@ -152,6 +152,26 @@ def get_diffusion_from_msd(structure, parameters, trajectory):
 
 
 @calcfunction
+def get_last_step_from_trajectory(trajectory):
+
+    if not isinstance(trajectory, orm.TrajectoryData):
+        raise Exception('All my inputs have to be instances of TrajectoryData')
+    traj = orm.TrajectoryData()
+    for arrname in trajectory.get_arraynames():
+        if arrname in ('symbols', 'atomic_species_name'):
+            traj.set_array(arrname, trajectory.get_array(arrname))
+        elif arrname in ('steps', 'times', 'walltimes'):
+            traj.set_array(arrname, np.array([trajectory.get_array(arrname)[0]]))
+        else:
+            traj.set_array(arrname, np.array([trajectory.get_array(arrname)[-1]]))
+
+    [traj.set_attribute(k, v) for k, v in trajectory.attributes_items() if not k.startswith('array|')]
+    if 'timestep_in_fs' in trajectory.attributes:
+        traj.set_attribute('sim_time_fs', traj.get_array('steps').size * trajectory.get_attribute('timestep_in_fs'))
+    return {'last_step_trajectory': traj}
+
+
+@calcfunction
 def get_structure_from_trajectory(trajectory, parameters, structure=None, settings=None):
     """
     Get a structure from a trajectory, given the step index.
